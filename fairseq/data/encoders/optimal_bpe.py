a8f091c2893cf79672b741cd8ea78f4967b64334
Amit Moryossef <amitmoryossef@gmail.com>
2022-07-14 18:27:38 +0200

    feat(optimal_bpe): use the better bpe package

--- fairseq/data/encoders/optimal_bpe.py
+++ fairseq/data/encoders/optimal_bpe.py
@@ -2,133 +2,10 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Dict, List
 from dataclasses import dataclass, field
 
 from fairseq.data.encoders import register_bpe
-
-import numpy as np
-import itertools as it
-
 from fairseq.data.encoders.subword_nmt_bpe import SubwordNMTBPE, SubwordNMTBPEConfig
-
-
-class OptimalBPETokenizer:
-    def __init__(self, vocabulary: Dict[str, int], separator: str, dropout: int = 0):
-        self.vocabulary = vocabulary
-        self.separator = separator
-
-        self.cache = {}
-        self.dropout = dropout
-
-    def semi_markov_segment(self, word: str, dropout=0):
-        """
-        semi-Markov dynamic program for shortest segmentation with max frequency
-        TODO: apply dropout, by removing some "codes" from the vocabulary, and unk-segmentation
-        """
-
-        N = len(word)
-
-        # dynamic-programming array
-        𝜷 = np.ones((N + 1, 2), dtype=int) * N
-        𝜷[0] = 0
-        𝜷[:, 1] = 0
-
-        # backpointers
-        bp = {0: -1}
-
-        for m in range(N + 1):
-            for n in range(m):
-                segment = word[n:m]
-                if segment in self.vocabulary:
-                    if 𝜷[n, 0] + 1 < 𝜷[m, 0]:
-                        𝜷[m, 0] = 𝜷[n, 0] + 1
-                        𝜷[m, 1] = 𝜷[n, 1] + self.vocabulary[segment]
-                        bp[m] = n
-                    elif 𝜷[m, 0] == 𝜷[n, 0] + 1:
-                        if 𝜷[n, 1] + self.vocabulary[segment] > 𝜷[m, 1]:
-                            𝜷[m, 1] = 𝜷[n, 1] + self.vocabulary[segment]
-                            bp[m] = n
-
-        # extract backpointers
-        ptr = N
-        segmentation = []
-        while ptr != 0:
-            last_bp = bp[ptr]
-            segmentation = [word[last_bp:ptr]] + segmentation
-            ptr = bp[ptr]
-
-        # make sure we didn't fuck it
-        # assert 𝜷[N, 1] == sum(map(lambda x: self.vocabulary[x], segmentation))
-        assert "".join(segmentation) == word
-
-        return segmentation, 𝜷[N, 0], 𝜷[N, 1]
-
-    def segment_word(self, word: str):
-        """
-        segment a single word, aware of cache
-        """
-        if not self.dropout and word in self.cache:
-            return self.cache[word]
-
-        for c in word:
-            if c not in self.vocabulary:
-                self.vocabulary[c] = self.vocabulary[c + ' '] = 1
-        self.vocabulary[' '] = 0
-
-        self.cache[word], _, _ = self.semi_markov_segment(word, dropout=self.dropout)
-        return self.cache[word]
-
-    def segment_words(self, words: List[str], dropout=0) -> List[str]:
-        """segment a sequence of words with BPE encoding"""
-        for word in words:
-            segments = self.segment_word(word + ' ')
-            for segment in segments[:-1]:
-                yield segment + self.separator
-
-            yield segments[-1].rstrip()
-
-    def segment(self, sentence: str):
-        """segmenta  single sentence (whitespace-tokenized string) with BPE encoding"""
-        segments = self.segment_words(sentence.strip('\r\n ').split())
-        return ' '.join(segments)
-
-    def brute_segment(self, sentence):
-        """
-        Brute-force method to compute the shortest segmentation
-        such that every segment is in the vocabulary
-        """
-        N = len(sentence)
-        min_len, max_freq = N, 0
-
-        for mask in it.product([0, 1], repeat=N):
-            segmentation = []
-            cur = ""
-            for i, c in zip(mask, sentence):
-                if i == 1:
-                    segmentation.append(cur)
-                    cur = c
-                else:
-                    cur += c
-            if cur != "":
-                segmentation.append(cur)
-
-            # check whether each segment is in the vocabulary
-            bad = False
-            for segment in segmentation:
-                if segment not in self.vocabulary:
-                    bad = True
-            if not bad:
-                freq = sum(map(lambda x: self.vocabulary[x], segmentation))
-                if len(segmentation) < min_len:
-                    min_len = len(segmentation)
-                    max_freq = freq
-                elif len(segmentation) == min_len:
-                    max_freq = max(max_freq, freq)
-
-            assert "".join(segmentation) == sentence, print(segmentation, mask)
-
-        return min_len, max_freq
 
 
 @dataclass
@@ -140,6 +17,13 @@ class OptimalBPEConfig(SubwordNMTBPEConfig):
 class OptimalBPE(SubwordNMTBPE):
     def __init__(self, cfg):
         super().__init__(cfg)
+
+        try:
+            from better_bpe import OptimalBPETokenizer
+        except ImportError:
+            raise ImportError(
+                "Please install better_bpe with: pip install better-bpe"
+            )
 
         self.tokenizer = OptimalBPETokenizer(self.get_vocabulary(), self.bpe.separator)
 
--- fairseq/data/encoders/optimal_bpe_test.py
+++ fairseq/data/encoders/optimal_bpe_test.py
@@ -2,70 +2,8 @@ import os
 import tempfile
 import unittest
 
-from .optimal_bpe import OptimalBPE, OptimalBPETokenizer, OptimalBPEConfig
+from .optimal_bpe import OptimalBPE, OptimalBPEConfig
 
-
-class OptimalBPETokenizerTestCase(unittest.TestCase):
-    def test_semi_markov_segment_bs_brute_segment(self):
-        vocabulary = {"a": 5, "b": 9, "c": 50, "ab": 11, "bc": 20, "cb": 31}
-        tokenizer = OptimalBPETokenizer(vocabulary, '-')
-        word = "abccbaba"
-
-        segmentation, min_len1, max_freq1 = tokenizer.semi_markov_segment(word)
-        min_len2, max_freq2 = tokenizer.brute_segment(word)
-
-        self.assertEqual(min_len1, min_len2)
-        self.assertEqual(max_freq1, max_freq2)
-
-    def test_semi_markov_every_segment_in_vocab(self):
-        vocabulary = {"a": 5, "b": 9, "c": 50, "ab": 11, "bc": 20, "cb": 31}
-        tokenizer = OptimalBPETokenizer(vocabulary, '-')
-        word = "abccbaba"
-
-        segmentation, _, _ = tokenizer.semi_markov_segment(word)
-        for s in segmentation:
-            self.assertTrue(s in vocabulary)
-
-    def test_semi_markov_segment_tokens_replicate_source(self):
-        vocabulary = {"a": 5, "b": 9, "c": 50, "ab": 11, "bc": 20, "cb": 31}
-        tokenizer = OptimalBPETokenizer(vocabulary, '-')
-        word = "abccbaba"
-
-        segmentation, _, _ = tokenizer.semi_markov_segment(word)
-
-        self.assertEqual("".join(segmentation), word)
-
-    def test_word_segmentation_cache_added(self):
-        vocabulary = {"a": 5, "b": 9, "c": 50, "ab": 11, "bc": 20, "cb": 31}
-        tokenizer = OptimalBPETokenizer(vocabulary, '-')
-        word = "abccbaba"
-        expected = ['ab', 'c', 'cb', 'ab', 'a']
-
-        segments = tokenizer.segment_word(word)
-        self.assertEqual(segments, expected)
-        self.assertEqual(tokenizer.cache, {word: expected})
-
-    def test_segment_words_multiple_words(self):
-        vocabulary = {"a ": 5, "a": 5, "b": 9, "c": 50, "ab ": 11,"ab": 11, "bc": 20, "cb": 31}
-        tokenizer = OptimalBPETokenizer(vocabulary, '-')
-        words = ["abccbaba", "bcaab"]
-        tokens = list(tokenizer.segment_words(words))
-        self.assertEqual(['ab-', 'c-', 'cb-', 'ab-', 'a', 'bc-', 'a-', 'ab'], tokens)
-
-    def test_sentence_segmentation(self):
-        vocabulary = {"a": 5, "b": 9, "c": 50, "ab": 11, "bc": 20, "cb": 31}
-        tokenizer = OptimalBPETokenizer(vocabulary, '-')
-        sentence = "abccbaba bcaab\n"
-        tokenized = tokenizer.segment(sentence)
-        self.assertEqual("ab- c- cb- ab- a bc- a- ab", tokenized)
-
-
-# def test_semi_markov_segment_unknown_tokens(self):
-#     vocabulary = {"a": 5, "a✡": 1, "b": 9, "c": 50, "ab": 11, "bc": 20, "cb": 31}
-#     tokenizer = OptimalBPETokenizer(vocabulary, '-')
-#     sentence = "abccbaba def"
-#     tokenized = tokenizer.segment(sentence)
-#     self.assertEqual(tokenized, "ab- c- cb- ab- a✡ bc- a- ab✡")
 
 class OptimalBPETestCase(unittest.TestCase):
     def test_sentence_segmentation(self):
@@ -74,11 +12,10 @@ class OptimalBPETestCase(unittest.TestCase):
             with open(path, 'w') as tmp:
                 tmp.writelines([
                     '#version: 0.2\n',
-                    'a ✡\n',
                     'a b\n',
                     'b c\n',
                     'c b\n',
-                    'a b✡\n',
+                    'a b</w>\n',
                 ])
             cfg = OptimalBPEConfig(bpe_codes=path)
             encoder = OptimalBPE(cfg)
@@ -87,7 +24,7 @@ class OptimalBPETestCase(unittest.TestCase):
 
         sentence = "abccbaba bcaab\n"
         tokenized = encoder.encode(sentence)
-        self.assertEqual(tokenized, "ab@@ c@@ cb@@ ab@@ a bc@@ a@@ ab")
+        self.assertEqual("ab@@ c@@ cb@@ ab@@ a bc@@ a@@ ab", tokenized)
 
 
 if __name__ == '__main__':
